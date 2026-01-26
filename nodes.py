39ebb0ac1d1af496342badb81c5b86f8064224ed
马骏驰 <majunchi1@xiaomi.com>
2026-01-26 10:35:15 +0800

    Support HeartMuLa/Codec 20260123 version

--- nodes.py
+++ nodes.py
@@ -29,7 +29,7 @@ class HeartMuLaLoader:
             "required": {
                 "mula_model": (folder_paths.get_filename_list("checkpoints"), ),
                 "codec_model": (folder_paths.get_filename_list("checkpoints"), ),
-                "version": (["3B-merged", "3B-20260123"], {"default": "3B-20260123"}),
+                "version": (["3B-merged", "3B-20260123"], {"default": "3B-merged"}),
                 "mula_device": (["cuda", "cpu"], {"default": "cuda"}),
                 "codec_device": (["cuda", "cpu"], {"default": "cuda"}),
                 "mula_dtype": (["bfloat16", "float16", "float32"], {"default": "bfloat16"}),
